24dc45962f1cb74d2b3c35627963e3d0f1bc4bf3
Alex Basystyi <alexbasisty@gmail.com>
2022-05-21 13:00:21 +0200

    checked wrong dates for non-31-day months

--- __tests__/formatDate.spec.ts
+++ __tests__/formatDate.spec.ts
@@ -9,15 +9,23 @@ describe('formatDate', () => {
         });
     });
 
-    it('returns date object for 2021-13-10', () => {
+    it('returns null for incorrect month 2021-13-10', () => {
         expect(formatDate('2021-13-10')).toBe(null);
     });
 
-    it('returns date object for 2021-10-32', () => {
+    it('returns null for incorrect date 2021-10-32', () => {
         expect(formatDate('2021-10-32')).toBe(null);
     });
 
-    it('returns date object for 1800-10-32', () => {
+    it('returns null for incorrect year 1800-10-32', () => {
         expect(formatDate('1800-10-21')).toBe(null);
+    });
+
+    it('returns null for incorrect date not in range for february 2000-02-30', () => {
+        expect(formatDate('2000-02-30')).toBe(null);
+    });
+
+    it('returns null for incorrect date not in range for 30-day months 2000-04-31', () => {
+        expect(formatDate('2000-04-31')).toBe(null);
     });
 })
